3fcebe9dfe8311155861fcfe9be39fb8f0761d9b
juanzunigap79 <juanzunigap79@gmail.com>
2024-10-16 14:24:41 +0000

    image error problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import vercel from '@astrojs/vercel/serverless';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: 'server',
-  adapter: vercel()
+  output: 'hybrid',
+  adapter: vercel(),
+  image: {
+    service: passthroughImageService()
+  }
 });
