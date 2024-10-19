fbed8fc50efc504765f38f27ffc1f447f43004f8
JuanZuniga79 <juanzunigap79@hotmail.com>
2024-10-19 18:23:30 -0500

    hard skill component done

--- src/modules/skills/data/hardSkills.ts
+++ src/modules/skills/data/hardSkills.ts
@@ -4,7 +4,9 @@ const getHardSkills = (lang: string) => lang === "en" ? en : es;
 
 const en: IHardSkill[] = [
     {section: "Languages", elements: [
-        {name: "Java", version: "11+", frequency: "almost every day", from: "08/2020", activity: 8},
+        {name: "java", version: "11+", frequency: "almost every day", from: "08/2020", activity: 8},
+        {name: "csharp", version: "10+", frequency: "every other day", from: "08/2021", activity: 7},
+        {name: "typescript", version: "4.6+", frequency: "every day", from: "03/2022", activity: 10},
     ]}
 ]
 
