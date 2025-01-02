67cd49b4a9526927b11c8ecfef9de0c7f9d70d9a
JuanZuniga79 <cd.juanzuniga@gmail.com>
2025-01-02 18:33:30 -0500

    optimizations and fixes

--- src/modules/home/data/services.ts
+++ src/modules/home/data/services.ts
@@ -4,14 +4,20 @@ import Server from "@core/assets/svg/server.svg"
 import FullStack from "@core/assets/svg/fullstack.svg"
 
 export const esServices: IService[] = [
-  {label: "Frontend", icon: Page,
+  {
+    label: "Frontend", icon: Page,
     description: "Desarrollo una amplia gama de soluciones web: sitios web informativos, plataformas dinámicas con " +
-      "gestión de contenido, aplicaciones web interactivas y soluciones en tiempo real para empresas y particulares."},
-  {label: "Backend", icon: Server,
+      "gestión de contenido, aplicaciones web interactivas y soluciones en tiempo real para empresas y particulares."
+  },
+  {
+    label: "Backend", icon: Server,
     description: "Desarrollo soluciones robustas de backend, con un fuerte enfoque en la seguridad y escalabilidad. " +
-      "Desde APIs RESTful y microservicios hasta sistemas complejos en tiempo real."},
-  {label: "FullStack", icon: FullStack,
+      "Desde APIs RESTful y microservicios hasta sistemas complejos en tiempo real."
+  },
+  {
+    label: "FullStack", icon: FullStack,
     description: "Ofrezco soluciones integrales de desarrollo web, abarcando desde el diseño de la interfaz de usuario " +
       "hasta la implementación de la lógica del servidor y la gestión de bases de datos. Mi enfoque full-stack me " +
-      "permite crear aplicaciones web completas y funcionales."}
+      "permite crear aplicaciones web completas y funcionales."
+  }
 ]
--- src/modules/home/services/getServices.ts
+++ src/modules/home/services/getServices.ts
@@ -1,4 +1,4 @@
-import {esServices} from "@home/data/services.tsx";
+import { esServices } from "@home/data/services.ts";
 
 export default async function getServices(lang: string) {
   return lang === "en" ? esServices : esServices;
